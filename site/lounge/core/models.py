9a38168ef56be8edf81d31e2995c08dcd6496a55
Tomoharu Yachikami <yachirus@localhost.localdomain>
2012-08-19 23:02:00 +0900

    Add core app.

--- site/lounge/core/models.py
+++ site/lounge/core/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Lounge(models.Model):
+    name = models.CharField(max_length = 256)
+    editors = models.ManyToManyField(User, related_name = 'lounges_belongs_to_as_editor')
+    members = models.ManyToManyField(User, related_name = 'lounges_belongs_to')
+
+class Topic(models.Model):
+    posted_by = models.ForeignKey(User)
+    date_time = models.DateTimeField(auto_now = True)
+    lounge_belongs_to = models.ForeignKey('Lounge')
+    tags = models.ManyToManyField('Tag', related_name = 'topics')
+    title = models.CharField(max_length = 256)
+    content = models.TextField()
+
+class Comment(models.Model):
+    content = models.TextField()
+
+class Tag(models.Model):
+    tag_name = models.CharField(max_length = 256)
