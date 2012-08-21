0452fe8d1d6172c1a4d1a93c066df1d5aa05a2c1
Tomoharu Yachikami <yachirus@gmail.com>
2012-08-21 22:35:33 +0900

    Add views

--- site/lounge/core/views.py
+++ site/lounge/core/views.py
@@ -1,1 +1,11 @@
+from django.shortcuts import render
+
 # Create your views here.
+def home(request):
+    return render(request, 'core/home.html')
+
+def lounge_list(request):
+    return render(request, 'core/loungelist.html')
+
+def lounge(request):
+    return render(request, 'core/lounge.html')
